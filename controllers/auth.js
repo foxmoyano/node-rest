af9b5767b3f61f9b95ab374e3a56bf7d900f0914
Luis Moyano <lmoyano@pragma.cl>
2023-04-11 22:33:20 -0400

    Fin proyecto rest-server - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { request, response } = require('express');
 const Usuario = require('../models/user');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generarJWT');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req = request, res = response ) =>  {
 
@@ -44,6 +45,50 @@ const login = async (req = request, res = response ) =>  {
     }
 };
 
+const googleSignIn = async( req, res = response ) => {
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+        
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'VENTAS_ROLE'
+            }
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });            
+    } catch ( error ) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El Token ID no se pudo verificar',
+            error
+        });
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 };
